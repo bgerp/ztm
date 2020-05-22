12f622f66ade248a15ce8b37e263617e32dcee60
Orlin Dimitrov <orlin369@gmail.com>
2020-05-22 11:15:18 +0300

    Add cross recource monitoring plugin

---
+++ Zontromat/plugins/monitoring/monitoring.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+
+Zontromat - Zonal Electronic Automation
+
+Copyright (C) [2020] [POLYGONTeam Ltd.]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import os
+
+from utils.logger import get_logger
+from utils.timer import Timer
+
+from plugins.base_plugin import BasePlugin
+
+from plugins.monitoring.monitoring_level import MonitoringLevel
+from plugins.monitoring.rule import Rule
+from plugins.monitoring.rules import Rules
+
+#region File Attributes
+
+__author__ = "Orlin Dimitrov"
+"""Author of the file."""
+
+__copyright__ = "Copyright 2020, POLYGON Team Ltd."
+"""Copyrighter
+@see http://polygonteam.com/"""
+
+__credits__ = ["Angel Boyarov, Zdravko Ivanov"]
+"""Credits"""
+
+__license__ = "GPLv3"
+"""License
+@see http://www.gnu.org/licenses/"""
+
+__version__ = "1.0.0"
+"""Version of the file."""
+
+__maintainer__ = "Orlin Dimitrov"
+"""Name of the maintainer."""
+
+__email__ = "or.dimitrov@polygonteam.com"
+"""E-mail of the author.
+@see or.dimitrov@polygonteam.com"""
+
+__status__ = "Debug"
+"""File status."""
+
+__name_space__ = "monitoring"
+
+#endregion
+
+class Monitoring(BasePlugin):
+    """Blinds controller device."""
+
+#region Attributes
+
+    __logger = None
+    """Logger"""
+
+    __update_timer = None
+    """Update timer."""
+
+    __rules = None
+    """Rules"""
+
+#endregion
+
+#region Private Methods
+
+    def __setup_rules(self):
+
+        self.__rules = Rules()
+
+        # GPIOs
+        for index in range(9):
+            self.__rules.add(Rule("DO{}".format(index), MonitoringLevel.Error))
+            self.__rules.add(Rule("DI{}".format(index), MonitoringLevel.Warning))
+            self.__rules.add(Rule("AO{}".format(index), MonitoringLevel.Error))
+            self.__rules.add(Rule("AI{}".format(index), MonitoringLevel.Warning))
+            self.__rules.add(Rule("RO{}".format(index), MonitoringLevel.Error))
+
+        # 1 Wire devices
+        ow_devices = self._controller.get_1w_devices()
+        for ow_device in ow_devices:
+            self.__rules.add(Rule(ow_device["circuit"], MonitoringLevel.Info))
+
+        # Serial Ports
+        if os.name == "nt":
+            self.__rules.add(Rule("COM4", MonitoringLevel.Error))
+            self.__rules.add(Rule("COM5", MonitoringLevel.Error))
+
+        elif os.name == "posix":
+            self.__rules.add(Rule("/dev/ttyUSB0", MonitoringLevel.Error))
+            self.__rules.add(Rule("/dev/ttyUSB1", MonitoringLevel.Error))
+
+        # Add event.
+        self.__rules.on_event(self.__on_event)
+
+    def __on_event(self, intersections, rule: Rule):
+
+        level = MonitoringLevel(rule.level)
+
+        if level == MonitoringLevel.Debug:
+            self.__logger.debug("Debug")
+            self.__logger.debug(intersections)
+
+        if level == MonitoringLevel.Info:
+            self.__logger.info("Info")
+            self.__logger.info(intersections)
+
+        if level == MonitoringLevel.Warning:
+            self.__logger.warning("Warning")
+            self.__logger.warning(intersections)
+
+        if level == MonitoringLevel.Error:
+            self.__logger.error("Error")
+            self.__logger.error(intersections)
+
+#endregion
+
+#region Public Methods
+
+    def init(self):
+        """Initialize the plugin."""
+
+        self.__logger = get_logger(__name__)
+        self.__logger.info("Starting the {} with name {}".format(__name__, self.name))
+
+        self.__update_timer = Timer(1)
+
+        self.__setup_rules()
+
+
+    def update(self):
+        """Runtime of the plugin."""
+
+        # Update the timer.
+        self.__update_timer.update()
+
+        if self.__update_timer.expired:
+
+            self.__update_timer.clear()
+
+            self.__rules.check(self._registers.to_dict())
+
+    def shutdown(self):
+        """Shutting down the blinds."""
+
+        self.__logger.info("Shutting down the {} with name {}".format(__name__, self.name))
+
+#endregion
---
+++ Zontromat/plugins/monitoring/tests/test.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+
+Zontromat - Zonal Electronic Automation
+
+Copyright (C) [2020] [POLYGONTeam Ltd.]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import json
+import unittest
+
+from monitoring.monitoring_level import MonitoringLevel
+from monitoring.rule import Rule
+from monitoring.rules import Rules
+
+#region File Attributes
+
+__author__ = "Orlin Dimitrov"
+"""Author of the file."""
+
+__copyright__ = "Copyright 2020, POLYGON Team Ltd."
+"""Copyrighter
+@see http://polygonteam.com/"""
+
+__credits__ = ["Angel Boyarov, Zdravko Ivanov"]
+"""Credits"""
+
+__license__ = "GPLv3"
+"""License
+@see http://www.gnu.org/licenses/"""
+
+__version__ = "1.0.0"
+"""Version of the file."""
+
+__maintainer__ = "Orlin Dimitrov"
+"""Name of the maintainer."""
+
+__email__ = "or.dimitrov@polygonteam.com"
+"""E-mail of the author.
+@see or.dimitrov@polygonteam.com"""
+
+__status__ = "Debug"
+"""File status."""
+
+#endregion
+
+class TestStringMethods(unittest.TestCase):
+    """Unit test of the system that is responsible for resource intersection detection."""
+
+#region Attributes
+
+    __rules = Rules()
+    """Rules"""
+
+    __rules_file_name = ""
+    """Rules filename."""
+
+#endregion
+
+#region Private Methods
+
+    def __on_event(self, intersections, rule):
+
+        level = MonitoringLevel(rule.level)
+
+        if level == MonitoringLevel.Debug:
+            print("Debug")
+
+        if level == MonitoringLevel.Info:
+            print("Info")
+
+        if level == MonitoringLevel.Warning:
+            print("Warning")
+
+        if level == MonitoringLevel.Error:
+            print("Error")
+
+        print(intersections)
+
+#endregion
+
+#region Unit Tests
+
+    def test_export_rules(self):
+
+        # GPIOs
+        for index in range(9):
+            self.__rules.add(Rule("DO{}".format(index), MonitoringLevel.Error))
+            self.__rules.add(Rule("DI{}".format(index), MonitoringLevel.Warning))
+            self.__rules.add(Rule("AO{}".format(index), MonitoringLevel.Error))
+            self.__rules.add(Rule("AI{}".format(index), MonitoringLevel.Warning))
+            self.__rules.add(Rule("RO{}".format(index), MonitoringLevel.Error))
+
+        # 1 Wire devices
+        self.__rules.add(Rule("26607314020000F8", MonitoringLevel.Info))
+        self.__rules.add(Rule("28FFFCD0001703AE", MonitoringLevel.Info))
+        self.__rules.add(Rule("28FFC4EE00170349", MonitoringLevel.Info))
+        self.__rules.add(Rule("28FF2B70C11604B7", MonitoringLevel.Info))
+
+        # Serial Ports
+        self.__rules.add(Rule("COM4", MonitoringLevel.Error))
+        self.__rules.add(Rule("COM5", MonitoringLevel.Error))
+
+        self.__rules.to_file("C://Users//POLYGONTeam//Documents//Python//json//out_rules.json")
+
+        self.assertEqual("foo".upper(), "FOO")
+
+    def test_load_rules(self):
+
+        self.__rules.from_file("C://Users//POLYGONTeam//Documents//Python//json//rules.json")
+
+        self.assertTrue("FOO".isupper())
+
+    def test_split(self):
+
+        self.__rules.on_event(self.__on_event)
+
+        registers = None
+
+        # Load content from file.
+        with open("C://Users//POLYGONTeam//Documents//Python//json//data.json", "r") as json_file:
+            registers = json.load(json_file)
+
+        self.__rules.check(registers)
+
+        self.assertFalse("Foo".isupper())
+
+#endregion
+
+if __name__ == "__main__":
+    unittest.main()
