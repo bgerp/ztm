018ea3a63b95f9cf535c78e6ce3834d0b3381382
Orlin Dimitrov <orlin369@gmail.com>
2020-06-03 14:25:57 +0300

    Add message clearing logic

--- Zontromat/plugins/monitoring/monitoring.py
+++ Zontromat/plugins/monitoring/monitoring.py
@@ -114,6 +114,7 @@ class Monitoring(BasePlugin):
         self.__rules.on_event(self.__on_event)
 
     def __on_event(self, intersections, rule: Rule):
+        """On event callback for collisions."""
 
         level = MonitoringLevel(rule.level)
 
@@ -122,16 +123,52 @@ class Monitoring(BasePlugin):
             self.__logger.debug(intersections)
 
         if level == MonitoringLevel.Info:
-            self.__logger.info("Info")
-            self.__logger.info(intersections)
+            # self.__logger.info("Info")
+            # self.__logger.info(intersections)
+
+            info_message = self._registers.by_name(self._key + ".info_message")
+            if info_message is not None:
+                info_message.value = str(intersections)
 
         if level == MonitoringLevel.Warning:
-            self.__logger.warning("Warning")
-            self.__logger.warning(intersections)
+            # self.__logger.warning("Warning")
+            # self.__logger.warning(intersections)
+
+            warning_message = self._registers.by_name(self._key + ".warning_message")
+            if warning_message is not None:
+                warning_message.value = str(intersections)
 
         if level == MonitoringLevel.Error:
-            self.__logger.error("Error")
-            self.__logger.error(intersections)
+            # self.__logger.error("Error")
+            # self.__logger.error(intersections)
+
+            error_message = self._registers.by_name(self._key + ".error_message")
+            if error_message is not None:
+                error_message.value = str(intersections)
+
+    def __clear_errors_cb(self, register):
+        """Clear errors callback."""
+
+        if register.value == 1:
+            # Clear the flag.
+            clear_errors = self._registers.by_name(self._key + ".clear_errors")
+            if clear_errors is not None:
+                clear_errors.value = 0
+
+            # Clear info messages.
+            info_message = self._registers.by_name(self._key + ".info_message")
+            if info_message is not None:
+                info_message.value = ""
+
+            # Clear warning messages.
+            warning_message = self._registers.by_name(self._key + ".warning_message")
+            if warning_message is not None:
+                warning_message.value = ""
+
+            # Clear error messages.
+            error_message = self._registers.by_name(self._key + ".error_message")
+            if error_message is not None:
+                error_message.value = ""
 
 #endregion
 
@@ -147,6 +184,9 @@ class Monitoring(BasePlugin):
 
         self.__setup_rules()
 
+        clear_errors = self._registers.by_name(self._key + ".clear_errors")
+        if clear_errors is not None:
+            clear_errors.update_handler = self.__clear_errors_cb
 
     def update(self):
         """Runtime of the plugin."""
