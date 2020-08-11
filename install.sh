539303e6f005bf9463520b9714248f1e125cf695
Orlin Dimitrov <orlin369@gmail.com>
2020-08-11 14:28:54 +0300

    Update install shell file

--- install.sh
+++ install.sh
@@ -1,38 +1,16 @@
 #!/bin/bash
-echo "Doing apt update and upgrade"
-apt update -y
-apt upgrade -y
 
-echo "Installing git and salt-minion"
-apt install git salt-minion -y
-
-echo "Adding unipi repositories"
-wget https://repo.unipi.technology/debian/unipi.list -O /etc/apt/sources.list.d/unipi.list
-wget https://repo.unipi.technology/debian/unipi_pub.gpg -O - | apt-key add -
-
-apt update -y
-
-apt-get install python3 neuron-kernel uuid-runtime unipi-firmware python3-pip unipi-modbus-tools -y
-
-UNIQUE_ID="$(uuidgen)"
-
-echo "Unique id generated: $UNIQUE_ID"
-
-echo "  master: 176.33.1.15" >> /etc/salt/minion
-
-echo "  id: $UNIQUE_ID" >> /etc/salt/minion
-
-systemctl enable salt-minion
-sh generate_settings.sh
 #Set timezone
 timedatectl set-timezone Europe/Sofia
 
+SERIAL_NUMBER=$(python3 /opt/Zontromat/info.py -s)
 
-#Setup evok
-apt-get install nginx -y
-rm -f /etc/nginx/sites-enabled/default
-apt-get install evok -y
-systemctl enable evok
-reboot
+cat /dev/null > /etc/salt/minion
 
+echo "  master: 109.199.153.86" >> /etc/salt/minion
 
+echo "  id: sn$SERIAL_NUMBER" >> /etc/salt/minion
+
+systemctl restart salt-minion
+sh generate_settings.sh
+echo "Done!"
