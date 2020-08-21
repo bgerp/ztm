c737f5891ddf89df59b80aa0bb7148e2a3146dbb
Orlin Dimitrov <orlin369@gmail.com>
2020-08-21 10:37:57 +0300

    Update instalation procedure

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-#Set timezone
-timedatectl set-timezone Europe/Sofia
+FILE=/var/log/firstboot
+#Check if booting for first time
+if [ ! -f "$FILE" ]; then
+  SERIAL_NUMBER=$(python3 /opt/Zontromat/info.py -s)
+  #clear the minion config
+  cat /dev/null >/etc/salt/minion
 
-SERIAL_NUMBER=$(python3 /opt/Zontromat/info.py -s)
+  echo "  master: 109.199.153.86" >>/etc/salt/minion
 
-cat /dev/null > /etc/salt/minion
+  echo "  id: sn$SERIAL_NUMBER" >>/etc/salt/minion
 
-echo "  master: 109.199.153.86" >> /etc/salt/minion
+  systemctl restart salt-minion
+  bash /opt/Zontromat/sh/generate_settings.sh
+  #Set timezone
+  timedatectl set-timezone Europe/Sofia
 
-echo "  id: sn$SERIAL_NUMBER" >> /etc/salt/minion
+  #set hostname
+  /opt/unipi/tools/unipihostname
 
-systemctl restart salt-minion
-sh generate_settings.sh
-echo "Done!"
+  #Make sure it would not run again
+  touch /var/log/firstboot
+fi
