44936375dd99c3b27a4571d2c08e72677dd708b6
Orlin Dimitrov <orlin369@gmail.com>
2020-11-26 16:06:39 +0200

    Add window tamper shutdown

--- Zontromat/plugins/hvac/hvac.py
+++ Zontromat/plugins/hvac/hvac.py
@@ -174,6 +174,9 @@ class HVAC(BasePlugin):
     __update_rate = 5
     """Update rate in seconds."""
 
+    __stop_flag = 0
+    """HVAC Stop flag."""
+
 #endregion
 
 #region Properties
@@ -223,7 +226,7 @@ class HVAC(BasePlugin):
     def __update_rate_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -233,7 +236,7 @@ class HVAC(BasePlugin):
     def __delta_time_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -255,7 +258,7 @@ class HVAC(BasePlugin):
     def __thermal_force_limit_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -265,7 +268,7 @@ class HVAC(BasePlugin):
     def __adjust_temp_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -299,7 +302,7 @@ class HVAC(BasePlugin):
     def __goal_building_temp_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -475,7 +478,7 @@ class HVAC(BasePlugin):
     def __loop1_fan_min_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -486,7 +489,7 @@ class HVAC(BasePlugin):
     def __loop1_fan_max_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -591,7 +594,7 @@ class HVAC(BasePlugin):
     def __loop2_fan_min_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -602,7 +605,7 @@ class HVAC(BasePlugin):
     def __loop2_fan_max_cb(self, register):
 
         # Check data type.
-        if not register.data_type == "float":
+        if not (register.data_type == "float" or register.data_type == "int"):
             GlobalErrorHandler.log_bad_register_value(self.__logger, register)
             return
 
@@ -674,6 +677,13 @@ class HVAC(BasePlugin):
     def __set_thermal_force(self, thermal_force):
         """ Apply thermal force to the devices. """
 
+        # 6. Ако модула на пределната термална сила е по-малък от модула на термалната сила,
+        # тогава Термалата сила = Пределната термала сила
+        if thermal_force > abs(self.__thermal_force_limit):
+            thermal_force = self.__thermal_force_limit
+        elif thermal_force < -abs(self.__thermal_force_limit):
+            thermal_force = -abs(self.__thermal_force_limit)
+
         # 7. Лимитираме Термалната сила в интервала -100 : + 100:
         if thermal_force < -100:
             thermal_force = -100
@@ -727,7 +737,7 @@ class HVAC(BasePlugin):
             # Set convector fan.
             conv_tf = l_scale(thermal_force, [0, 100], [0, 3])
             conv_tf = int(conv_tf)
-            # self.__convector_dev.set_state(abs(conv_tf))
+            self.__convector_dev.set_state(abs(conv_tf))
 
 #endregion
 
@@ -751,95 +761,116 @@ class HVAC(BasePlugin):
         update_rate = self._registers.by_name(self._key + ".update_rate")
         if update_rate is not None:
             update_rate.update_handlers = self.__update_rate_cb
+            update_rate.update()
 
         delta_time = self._registers.by_name(self._key + ".delta_time")
         if delta_time is not None:
             delta_time.update_handlers = self.__delta_time_cb
+            delta_time.update()
 
         thermal_mode = self._registers.by_name(self._key + ".thermal_mode")
         if thermal_mode is not None:
             thermal_mode.update_handlers = self.__thermal_mode_cb
+            thermal_mode.update()
 
         thermal_force_limit = self._registers.by_name(self._key + ".thermal_force_limit")
         if thermal_force_limit is not None:
             thermal_force_limit.update_handlers = self.__thermal_force_limit_cb
+            thermal_force_limit.update()
 
         adjust_temp = self._registers.by_name(self._key + ".adjust_temp")
         if adjust_temp is not None:
             adjust_temp.update_handlers = self.__adjust_temp_cb
+            adjust_temp.update()
 
         goal_building_temp = self._registers.by_name(self._key + ".goal_building_temp")
         if goal_building_temp is not None:
             goal_building_temp.update_handlers = self.__goal_building_temp_cb
+            goal_building_temp.update()
 
         # Air temperatures.
         air_temp_cent_enabled = self._registers.by_name(self._key + ".air_temp_cent.settings")
         if air_temp_cent_enabled is not None:
             air_temp_cent_enabled.update_handlers = self.__air_temp_cent_settings_cb
+            air_temp_cent_enabled.update()
 
         air_temp_lower_enabled = self._registers.by_name(self._key + ".air_temp_lower.settings")
         if air_temp_lower_enabled is not None:
             air_temp_lower_enabled.update_handlers = self.__air_temp_lower_settings_cb
+            air_temp_lower_enabled.update()
 
         air_temp_upper_enabled = self._registers.by_name(self._key + ".air_temp_upper.settings")
         if air_temp_upper_enabled is not None:
             air_temp_upper_enabled.update_handlers = self.__air_temp_upper_settings_cb
+            air_temp_upper_enabled.update()
 
         # Convector
         convector_enable = self._registers.by_name(self._key + ".convector.settings")
         if convector_enable is not None:
             convector_enable.update_handlers = self.__convector_settings_cb
+            convector_enable.update()
 
         # Loop 1
         loop1_cnt_enabled = self._registers.by_name(self._key + ".loop1.cnt.input")
         if loop1_cnt_enabled is not None:
             loop1_cnt_enabled.update_handlers = self.__loop1_cnt_input_cb
+            loop1_cnt_enabled.update()
 
         loop1_fan_enabled = self._registers.by_name(self._key + ".loop1.fan.settings")
         if loop1_fan_enabled is not None:
             loop1_fan_enabled.update_handlers = self.__loop1_fan_settings_cb
+            loop1_fan_enabled.update()
 
         loop1_fan_min = self._registers.by_name(self._key + ".loop1.fan.min_speed")
         if loop1_fan_min is not None:
             loop1_fan_min.update_handlers = self.__loop1_fan_min_cb
+            loop1_fan_min.update()
 
         loop1_fan_max = self._registers.by_name(self._key + ".loop1.fan.max_speed")
         if loop1_fan_max is not None:
             loop1_fan_max.update_handlers = self.__loop1_fan_max_cb
+            loop1_fan_max.update()
 
         loop1_temp_enabled = self._registers.by_name(self._key + ".loop1.temp.settings")
         if loop1_temp_enabled is not None:
             loop1_temp_enabled.update_handlers = self.__loop1_temp_settings_cb
+            loop1_temp_enabled.update()
 
         loop1_valve_enabled = self._registers.by_name(self._key + ".loop1.valve.settings")
         if loop1_valve_enabled is not None:
             loop1_valve_enabled.update_handlers = self.__loop1_valve_enabled_cb
+            loop1_valve_enabled.update()
 
         # Loop 2
         loop2_cnt_enabled = self._registers.by_name(self._key + ".loop2.cnt.input")
         if loop2_cnt_enabled is not None:
             loop2_cnt_enabled.update_handlers = self.__loop2_cnt_input_cb
+            loop1_cnt_enabled.update()
 
         loop2_fan_enabled = self._registers.by_name(self._key + ".loop2.fan.settings")
         if loop2_fan_enabled is not None:
             loop2_fan_enabled.update_handlers = self.__loop2_fan_settings_cb
+            loop2_fan_enabled.update()
 
         loop2_fan_min = self._registers.by_name(self._key + ".loop2.fan.min_speed")
         if loop2_fan_min is not None:
             loop2_fan_min.update_handlers = self.__loop2_fan_min_cb
+            loop2_fan_min.update()
 
         loop2_fan_max = self._registers.by_name(self._key + ".loop2.fan.max_speed")
         if loop2_fan_max is not None:
             loop2_fan_max.update_handlers = self.__loop2_fan_max_cb
+            loop2_fan_max.update()
 
         loop2_temp_enabled = self._registers.by_name(self._key + ".loop2.temp.settings")
         if loop2_temp_enabled is not None:
             loop2_temp_enabled.update_handlers = self.__loop2_temp_settings_cb
+            loop2_temp_enabled.update()
 
         loop2_valve_enabled = self._registers.by_name(self._key + ".loop2.valve.settings")
         if loop2_valve_enabled is not None:
             loop2_valve_enabled.update_handlers = self.__loop2_valve_settings_cb
-
+            loop2_valve_enabled.update()
 
         # Shutting down all the devices.
         # self.__set_thermal_force(0)
@@ -847,14 +878,35 @@ class HVAC(BasePlugin):
     def update(self):
         """ Update cycle. """
 
+        # If there is no one at the zone, just turn off the lights.
+        ac_zone_occupied_flag = 0
+        ac_zone_occupied = self._registers.by_name("ac.zone_occupied")
+        if ac_zone_occupied is not None:
+            ac_zone_occupied_flag = ac_zone_occupied.value
+
+        # If the window is opened, just turn off the HVAC.
+        window_closed_1_state = False
+        gpio = self._registers.by_name("ac.window_closed_1.input")
+        if gpio is not None:
+            window_closed_1_state = self._controller.digital_read(gpio.value)
+            self._registers.write("ac.window_closed_1.state", window_closed_1_state)
+
+        stop_flag = (ac_zone_occupied_flag or not window_closed_1_state)
+
+        if self.__stop_flag != stop_flag:
+            self.__stop_flag = stop_flag
+
+            if self.__stop_flag:
+                self.__set_thermal_force(0)
+
         # Main update rate at ~ 20 second.
         # На всеки 20 секунди се правят следните стъпки:
         self.__update_timer.update()
-        if self.__update_timer.expired:
+        if self.__update_timer.expired and self.__stop_flag == 0:
             self.__update_timer.clear()
 
             # Recalculate the temperatures.
-            self.__temp_proc.update()
+            self.__temp_proc.update() # Add thermometers : DS18B20 - 28B802B5030000CF 11.8 °C
 
             crg_temp = 0
             expected_room_temp = 0
@@ -875,6 +927,7 @@ class HVAC(BasePlugin):
             # Целевата температура и Очакваната температура на стаята:
             crg_temp = goal_room_temp - expected_room_temp
             self.__logger.debug("CRG: {:3.3f}".format(crg_temp))
+
             # 4.
             # Колкото е по-отрицателна температурата, толкова повече трябва да охлаждаме,
             # колкото е по-положителна - толкова повече трябва да отопляваме.
@@ -884,13 +937,7 @@ class HVAC(BasePlugin):
             # 5. Интегрираме термалната сила:
             self.__thermal_force += crg_temp
 
-            # 6. Ако модула на пределната термална сила е по-малък от модула на термалната сила,
-            # тогава Термалата сила = Пределната термала сила
-            if self.__thermal_force > abs(self.__thermal_force_limit):
-                self.__thermal_force = self.__thermal_force_limit
-            elif self.__thermal_force < -abs(self.__thermal_force_limit):
-                self.__thermal_force = -abs(self.__thermal_force_limit)
-
+            # Apply the integrated force.
             self.__set_thermal_force(self.__thermal_force)
 
             if self.__loop1_valve_dev is not None:
@@ -914,20 +961,11 @@ class HVAC(BasePlugin):
         #     # Update current time.
         #     self.__lastupdate_delta_time = time.time()
 
-        # If there is no one at the zone, just turn off the lights.
-        ac_zone_occupied = self._registers.by_name("ac.zone_occupied")
-        if ac_zone_occupied is not None:
-            if ac_zone_occupied.value == 1:
-                self.__logger.debug("Just turn off the HVAC in the zone.")
-            if ac_zone_occupied.value == 0:
-                # TODO: Pass, but when activity has turnback return to normal state.
-                pass
-
     def shutdown(self):
         """Shutdown the tamper."""
 
         self.__logger.info("Shutting down the {}".format(self.name))
         self.__thermal_force = 0
-        self.__set_thermal_force(self.__thermal_force)
+        self.__set_thermal_force(0)
 
 #endregion
