b2ea3625fa1ba1e46a33f03e6866de81432f1857
Orlin Dimitrov <orlin369@gmail.com>
2021-04-14 17:15:15 +0300

    Add SUPER S8-3CN remote IO modbus support

---
+++ Zontromat/devices/super/s8_3cn/s8_3cn.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+
+Zontromat - Zonal Electronic Automation
+
+Copyright (C) [2020] [POLYGONTeam Ltd.]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from devices.drivers.modbus.device import Device
+from devices.drivers.modbus.parameter import Parameter
+from devices.drivers.modbus.parameter_type import ParameterType
+from devices.drivers.modbus.register_type import RegisterType
+
+#region File Attributes
+
+__author__ = "Orlin Dimitrov"
+"""Author of the file."""
+
+__copyright__ = "Copyright 2020, POLYGON Team Ltd."
+"""Copyrighter
+@see http://polygonteam.com/"""
+
+__credits__ = ["Angel Boyarov"]
+"""Credits"""
+
+__license__ = "GPLv3"
+"""License
+@see http://www.gnu.org/licenses/"""
+
+__version__ = "1.0.0"
+"""Version of the file."""
+
+__maintainer__ = "Orlin Dimitrov"
+"""Name of the maintainer."""
+
+__email__ = "or.dimitrov@polygonteam.com"
+"""E-mail of the author.
+@see or.dimitrov@polygonteam.com"""
+
+__status__ = "Debug"
+"""File status."""
+
+#endregion
+
+class S8_3CN(Device):
+    """This class is dedicated to read/write data from S8-3CN GPIO expander.
+
+    See: http://bends.se/?page=notebook/hardware/gecon-tcp-508n"""
+
+#region Constructor
+
+    def __init__(self):
+        """Constructor"""
+
+        self._parameters.append(
+            Parameter(
+                "ReadRelayOutputs",
+                "RelayOutputs",
+                ParameterType.INT16_T,
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                RegisterType.ReadCoil
+            )
+        )
+
+        self._parameters.append(
+            Parameter(
+                "ReadDigitalInputs",
+                "DigitalInputs",
+                ParameterType.INT16_T,
+                [0, 1, 2, 3, 4, 5, 6, 7],
+                RegisterType.ReadDiscreteInput
+            )
+        )
+
+        self._parameters.append(
+            Parameter(
+                "ReadAnalogOutputs",
+                "AnalogOutputs",
+                ParameterType.INT16_T,
+                [0, 1, 2, 3],
+                RegisterType.ReadHoldingRegisters
+            )
+        )
+
+        self._parameters.append(
+            Parameter(
+                "ReadAnalogInputs",
+                "AnalogInputs",
+                ParameterType.INT16_T,
+                [0, 1, 2, 3, 4, 5, 6, 7],
+                RegisterType.ReadInputRegisters
+            )
+        )
+
+
+        self._parameters.append(
+            Parameter(
+                "WriteAnalogOutputs",
+                "AnalogOutputs",
+                ParameterType.INT16_T,
+                [0, 1, 2, 3],
+                RegisterType.WriteMultipleHoldingRegisters
+            )
+        )
+
+        self._parameters.append(
+            Parameter(
+                "WriteRelayOutputs",
+                "RelayOutputs",
+                ParameterType.INT16_T,
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                RegisterType.WriteMultipleCoils
+            )
+        )
+
+#endregion
