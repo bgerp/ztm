d0e10055be23e0da90d28c23f496795f78a5a2dd
Orlin Dimitrov <orlin369@gmail.com>
2021-05-12 10:40:55 +0300

    Add thermal sensor interface and factory

---
+++ Zontromat/devices/utils/thermal_sensor/base_thermal_sensor.py
@@ -0,0 +1,92 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+
+Zontromat - Zonal Electronic Automation
+
+Copyright (C) [2020] [POLYGONTeam Ltd.]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from devices.base_device import BaseDevice
+
+#region File Attributes
+
+__author__ = "Orlin Dimitrov"
+"""Author of the file."""
+
+__copyright__ = "Copyright 2020, POLYGON Team Ltd."
+"""Copyrighter
+@see http://polygonteam.com/"""
+
+__credits__ = ["Angel Boyarov"]
+"""Credits"""
+
+__license__ = "GPLv3"
+"""License
+@see http://www.gnu.org/licenses/"""
+
+__version__ = "1.0.0"
+"""Version of the file."""
+
+__maintainer__ = "Orlin Dimitrov"
+"""Name of the maintainer."""
+
+__email__ = "or.dimitrov@polygonteam.com"
+"""E-mail of the author.
+@see or.dimitrov@polygonteam.com"""
+
+__status__ = "Debug"
+"""File status."""
+
+#endregion
+
+class BaseThermalSensor(BaseDevice):
+    """Card reader base class."""
+
+#region Attributes
+
+#endregion
+
+#region Constructor
+
+    def __init__(self, config):
+
+        super().__init__(config)
+
+#endregion
+
+#region Properties
+
+#endregion
+
+#region Public Methods
+
+    def get_temp(self):
+        """Get temperature.
+        """
+
+        pass
+
+    def get_hum(self):
+        """Get humidity.
+        """
+
+        pass
+
+#endregion
