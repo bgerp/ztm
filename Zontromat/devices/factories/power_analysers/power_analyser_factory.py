50b365658646b163990d406a70b6f8ff582bc1d3
Orlin Dimitrov <orlin369@gmail.com>
2021-05-13 18:16:57 +0300

    Create power analyser

---
+++ Zontromat/devices/factories/power_analysers/power_analyser_factory.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+
+Zontromat - Zonal Electronic Automation
+
+Copyright (C) [2020] [POLYGONTeam Ltd.]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from devices.vendors.Eastron.sdm120.sdm120 import SDM120
+from devices.vendors.Eastron.sdm630.sdm630 import SDM630
+
+#region File Attributes
+
+__author__ = "Orlin Dimitrov"
+"""Author of the file."""
+
+__copyright__ = "Copyright 2020, POLYGON Team Ltd."
+"""Copyrighter
+@see http://polygonteam.com/"""
+
+__credits__ = ["Angel Boyarov"]
+"""Credits"""
+
+__license__ = "GPLv3"
+"""License
+@see http://www.gnu.org/licenses/"""
+
+__version__ = "1.0.0"
+"""Version of the file."""
+
+__maintainer__ = "Orlin Dimitrov"
+"""Name of the maintainer."""
+
+__email__ = "or.dimitrov@polygonteam.com"
+"""E-mail of the author.
+@see or.dimitrov@polygonteam.com"""
+
+__status__ = "Debug"
+"""File status."""
+
+#endregion
+
+class PowerAnalyserFactory:
+
+    @staticmethod
+    def create(**config):
+        """Create card reader instace."""
+
+        # The card device.
+        device = None
+
+        # Name
+        name = None
+        if "name" in config:
+            name = config["name"]
+
+        # Vendor
+        vendor = None
+        if "params" in config:
+            vendor = config["params"][0]
+
+        else:
+            raise ValueError("No \"vendor\" argument has been passed.") 
+
+        # Model
+        model = None
+        if "params" in config:
+            model = config["params"][1].upper()
+
+        else:
+            raise ValueError("No \"model\" argument has been passed.") 
+
+        # Unit
+        unit = None
+        if "params" in config:
+            unit = int(config["params"][3])
+
+        else:
+            raise ValueError("No \"unit\" argument has been passed.") 
+
+
+        # Controller
+        controller = None
+        if "controller" in config:
+            controller = config["controller"]
+
+        else:
+            raise ValueError("No \"controller\" argument has been passed.") 
+
+
+        # Eastron / SDM120
+        if vendor == "Eastron" and  model == "SDM120":
+
+            device = SDM120(controller=controller,
+                name=name,
+                unit=unit)
+
+        # Eastron / ACR122
+        elif vendor == "Eastron" and model == "SDM630":
+            
+            device = SDM630(controller=controller,
+                name=name,
+                unit=unit)
+
+        else:
+            raise NotImplementedError("The {} and {}, is not supported.".format(vendor,model))
+
+        # Return the device.
+        return device
