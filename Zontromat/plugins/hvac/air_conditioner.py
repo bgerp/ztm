5e53d3d259c695d804e2809b791d3b952e75f244
Orlin Dimitrov <orlin369@gmail.com>
2021-07-06 18:58:52 +0300

    Add empty line

--- Zontromat/plugins/hvac/air_conditioner.py
+++ Zontromat/plugins/hvac/air_conditioner.py
@@ -21,6 +21,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
+
 from collections import deque
 
 from utils.logger import get_logger
