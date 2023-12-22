5ebdafc810f17991750ca5dc7591555b27e1781e
Orlin Dimitrov <orlin369@gmail.com>
2023-12-22 12:12:50 +0200

    Slowdown measurements, stop convector 1

--- Zontromat/plugins/hvac/zone.py
+++ Zontromat/plugins/hvac/zone.py
@@ -1929,8 +1929,8 @@ class Zone(BasePlugin):
 
             elif self.__experimental_counter == 15:
                 if self.__cl_1_vlv_dev is not None:
-                    self.__cl_1_vlv_dev.target_position = 100
-                    self.__conv_1_dev.set_state(1)
+                    # self.__cl_1_vlv_dev.target_position = 100
+                    # self.__conv_1_dev.set_state(1)
                     pass
 
             elif self.__experimental_counter == 20:
@@ -1997,7 +1997,7 @@ class Zone(BasePlugin):
         # Only for test.
         self.__experimental_update_timer = Timer(1)
 
-        self.__hm_demand_timer = Timer(5) # 3600
+        self.__hm_demand_timer = Timer(3600)
 
         # Create temperature processor.
         self.__temp_proc = TemperatureProcessor()
