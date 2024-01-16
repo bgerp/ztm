9df49bd2589be2cade292674eff85fdc6c2b0db1
Orlin Dimitrov <orlin369@gmail.com>
2024-01-16 12:38:45 +0200

    Change data type of flow metwer parameter

--- Zontromat/devices/vendors/smii/mw_uml_15/mw_uml_15.py
+++ Zontromat/devices/vendors/smii/mw_uml_15/mw_uml_15.py
@@ -74,7 +74,7 @@ class MW_UML_15(ModbusDevice):
 
         self._parameters.append(
             Parameter("CumulativeTraffic", "mL",\
-            ParameterType.UINT32_T_LE, [0x207, 0x208], FunctionCode.ReadHoldingRegisters))
+            ParameterType.FLOAT, [0x207, 0x208], FunctionCode.ReadHoldingRegisters))
 
         self._parameters.append(
             Parameter("InstantaneousFlow", "mL/h",\
