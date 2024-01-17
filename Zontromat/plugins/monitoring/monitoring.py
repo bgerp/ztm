589de3e4f2b2d4db817c51c52d62f1410cb5db0c
Orlin Dimitrov <orlin369@gmail.com>
2024-01-17 14:36:43 +0200

    Scale to units of the flow meter

--- Zontromat/plugins/monitoring/monitoring.py
+++ Zontromat/plugins/monitoring/monitoring.py
@@ -214,6 +214,18 @@ class Monitoring(BasePlugin):
                         pass
                     measurement[item] = self.__cw_flowmeter_dev.get_value(item)
 
+                    # Scale unit from milli liter to cubic meter. 
+                    if item == "CumulativeTraffic":
+                        measurement[item] /= 1000000.0
+
+                    # Scale to degrees by Celsius.
+                    if item == "WaterTemperature":
+                        measurement[item] /= 100.0
+
+                    # Scale to voltage.
+                    if item == "BatteryVoltage":
+                        measurement[item] /= 100.0
+
         else:
             self.__logger.error("Unknown power water meter")
 
