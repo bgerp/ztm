1060196f25cbbc130f5031abb917eeda48e47b75
Orlin Dimitrov <orlin369@gmail.com>
2024-01-22 20:01:42 +0200

    Add four states

--- Zontromat/plugins/hvac/zone.py
+++ Zontromat/plugins/hvac/zone.py
@@ -107,7 +107,7 @@ class PWMTimer():
         self.__lower_limit = 0
         self.__counter = 0
         self.__transition = 0
-        self.__state = False
+        self.__state = 0
         self.set_step(step)
         self.set_pwm(0)
 
@@ -134,29 +134,37 @@ class PWMTimer():
             self.__timer.clear()
 
             if self.__lower_limit <= self.__transition:
-                print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
-                print("Turn OFF by 0 duty cycle")
-                # if self.__toff_cb is not None:
-                #     self.__toff_cb()
+                if self.__state != 1:
+                    self.__state = 1
+                    print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
+                    print("Turn OFF by 0 duty cycle")
+                    # if self.__toff_cb is not None:
+                    #     self.__toff_cb()
 
             if self.__lower_limit == self.__counter and\
                 self.__counter <= self.__transition:
-                print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
-                print("Turn ON by Start period")
-                # if self.__ton_cb is not None:
-                #     self.__ton_cb()
+                if self.__state != 2:
+                    self.__state = 2
+                    print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
+                    print("Turn ON by Start period")
+                    # if self.__ton_cb is not None:
+                    #     self.__ton_cb()
 
             if self.__counter == self.__transition:
-                print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
-                print("Turn OFF by Stop period.")
-                # if self.__toff_cb is not None:
-                #     self.__toff_cb()
+                if self.__state != 3:
+                    self.__state = 3
+                    print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
+                    print("Turn OFF by Stop period.")
+                    # if self.__toff_cb is not None:
+                    #     self.__toff_cb()
 
             if self.__transition >= self.__upper_limit:
-                print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
-                print("Turn ON by 100 duty cycle.")
-                # if self.__ton_cb is not None:
-                #     self.__ton_cb()
+                if self.__state != 4:
+                    self.__state = 4
+                    print(f"L: {self.__lower_limit} C: {self.__counter} U: {self.__upper_limit} T: {self.__transition} S: {self.__step}")
+                    print("Turn ON by 100 duty cycle.")
+                    # if self.__ton_cb is not None:
+                    #     self.__ton_cb()
 
 
             # Increment timer.
