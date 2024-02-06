52cdf90d4daee0fdad6e7bec066468727fa787e2
Orlin Dimitrov <orlin369@gmail.com>
2024-02-06 16:20:00 +0200

    Update weather registers

--- Zontromat/export_registers.py
+++ Zontromat/export_registers.py
@@ -106,10 +106,10 @@ def __set_parser():
 
     # Add arguments.
     __parser.add_argument("--action", type=str, default="w_json", help="Export JSON file.")
-    # parser.add_argument("--action", type=str, default="w_csv", help="Export CSV file.")
-    # parser.add_argument("--action", type=str, default="list_gpio", help="Export type.")
-    # parser.add_argument("--action", type=str, default="w_md", help="Export MD file.")
-    # parser.add_argument("--path", type=str, default=file_name, help="Target file path.")
+    # __parser.add_argument("--action", type=str, default="w_csv", help="Export CSV file.")
+    # __parser.add_argument("--action", type=str, default="list_gpio", help="Export type.")
+    # __parser.add_argument("--action", type=str, default="w_md", help="Export MD file.")
+    # __parser.add_argument("--path", type=str, default=file_name, help="Target file path.")
 
     # Add args parameters
     __parser.add_argument("--pa", type=int, default=1, help="Power analyzer modbus ID.")
@@ -966,90 +966,15 @@ def __add_registers(args):
     register.value = 3600
     __registers.append(register)
 
-
-    register = Register("envm.temp.actual")
+    register = Register("envm.forecast.icon_0")
     register.scope = Scope.System
     register.plugin_name = "Environment"
-    register.description = "Actual outside temperature [C]"
-    register.range = "-50/50"
+    register.description = "Actual weather icon."
+    register.range = __range["NONE"]
     register.value = 0.0
     __registers.append(register)
 
-    register = Register("envm.temp.a3")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Actual outside temperature for 3 hours [C]"
-    register.range = "-50/50"
-    register.value = 0.0
-    __registers.append(register)
-
-    register = Register("envm.temp.a6")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Actual outside temperature for 6 hours [C]"
-    register.range = "-50/50"
-    register.value = 0.0
-    __registers.append(register)
-
-    register = Register("envm.temp.min24")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Minimum outside temperature for 24 hours [C]"
-    register.range = "-50/50"
-    register.value = 0.0
-    __registers.append(register)
-
-    register = Register("envm.temp.max24")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Maximum outside temperature for 24 hours [C]"
-    register.range = "-50/50"
-    register.value = 0.0
-    __registers.append(register)
-
-
-    register = Register("envm.wind.actual")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Actual wind now [m/sec]"
-    register.range = "0.0/"
-    register.value = 0.0
-    __registers.append(register)
-
-    register = Register("envm.wind.a3")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Actual wind for 3 hours [m/sec]"
-    register.range = "0.0/"
-    register.value = 0.0
-    __registers.append(register)
-
-    register = Register("envm.wind.a6")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Actual wind for 6 hours [m/sec]"
-    register.range = "0.0/"
-    register.value = 0.0
-    __registers.append(register)
-
-    register = Register("envm.wind.min24")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Minimum wind for 24 hours [m/sec]"
-    register.range = "0.0/"
-    register.value = 0.0
-    __registers.append(register)
-
-    register = Register("envm.wind.max24")
-    register.scope = Scope.System
-    register.plugin_name = "Environment"
-    register.description = "Maximum wind for 24 hours [m/sec]"
-    register.range = "0.0/"
-    register.value = 0.0
-    __registers.append(register)
-
-
-    register = Register("envm.rh.actual")
+    register = Register("envm.forecast.rh_0")
     register.scope = Scope.System
     register.plugin_name = "Environment"
     register.description = "Actual outside relative humidity [%]"
@@ -1057,22 +982,85 @@ def __add_registers(args):
     register.value = 0.0
     __registers.append(register)
 
-    register = Register("envm.rh.a3")
+    register = Register("envm.forecast.temp_0")
     register.scope = Scope.System
     register.plugin_name = "Environment"
-    register.description = "Actual outside relative humidity for 3 hours [%]"
+    register.description = "Actual outside temperature [*C]"
+    register.range = "-50.0/50.0"
+    register.value = 0.0
+    __registers.append(register)
+
+    register = Register("envm.forecast.wind_0")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside wind speed [m/s]"
     register.range = __range["PERCENTAGE_F"]
     register.value = 0.0
     __registers.append(register)
 
-    register = Register("envm.rh.a6")
+    register = Register("envm.forecast.icon_3")
     register.scope = Scope.System
     register.plugin_name = "Environment"
-    register.description = "Actual outside relative humidity for 6 hours [%]"
+    register.description = "Actual outside weather icon for 3 hours."
+    register.range = __range["NONE"]
+    register.value = 0.0
+    __registers.append(register)
+
+    register = Register("envm.forecast.rh_3")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside relative humidity for 3 hours.[%]"
     register.range = __range["PERCENTAGE_F"]
     register.value = 0.0
     __registers.append(register)
 
+    register = Register("envm.forecast.temp_3")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside temperature for 3 hours. [*C]"
+    register.range = "-50.0/50.0"
+    register.value = 0.0
+    __registers.append(register)
+
+    register = Register("envm.forecast.wind_3")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside wind speed for 3 hours. [m/s]"
+    register.range = __range["PERCENTAGE_F"]
+    register.value = 0.0
+    __registers.append(register)
+
+    register = Register("envm.forecast.icon_6")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside weather icon for 6 hours."
+    register.range = __range["NONE"]
+    register.value = 0.0
+    __registers.append(register)
+
+    register = Register("envm.forecast.rh_6")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside relative humidity for 6 hours.[%]"
+    register.range = __range["PERCENTAGE_F"]
+    register.value = 0.0
+    __registers.append(register)
+
+    register = Register("envm.forecast.temp_6")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside temperature for 6 hours. [*C]"
+    register.range = "-50.0/50.0"
+    register.value = 0.0
+    __registers.append(register)
+
+    register = Register("envm.forecast.wind_6")
+    register.scope = Scope.System
+    register.plugin_name = "Environment"
+    register.description = "Actual outside wind speed for 6 hours. [m/s]"
+    register.range = __range["PERCENTAGE_F"]
+    register.value = 0.0
+    __registers.append(register)
 
     register = Register("envm.light")
     register.scope = Scope.System
