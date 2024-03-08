d752d3aaac6c398d64256775ba4e5ad1c6719bd3
Orlin Dimitrov <orlin369@gmail.com>
2024-03-08 20:22:41 +0200

    Update weather DB to 10 minutes

--- Zontromat/zone.py
+++ Zontromat/zone.py
@@ -506,7 +506,7 @@ class Zone():
                 timeout = self.__app_settings.ui["timeout"])
 
             self.__ztm_ui_ut = Timer(1)
-            self.__ztm_ui_weather_cast_ut = Timer(150)
+            self.__ztm_ui_weather_cast_ut = Timer(600)
             self.__ztm_ui_heartbeat_ut = Timer(3600)
 
             # Log in.
