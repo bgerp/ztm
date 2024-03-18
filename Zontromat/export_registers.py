79945fbf7c4cf0ace120550b5c119847f2dd6d1f
Orlin Dimitrov <orlin369@gmail.com>
2024-03-18 12:38:21 +0200

    Set IO signals for conv east valves and set IO signals for conv east pump

--- Zontromat/export_registers.py
+++ Zontromat/export_registers.py
@@ -3140,10 +3140,10 @@ def __add_registers(args):
                     "close_on_shutdown": True,
                     "wait_on_shutdown": False,
                     "io_mode": 1, # 1: "single_out", 2: "dual_out"
-                    "output_cw": "U0:ID2:FC5:R0:RO6",
+                    "output_cw": "U0:ID16:FC5:R0:RO4",
                     "output_ccw": "off",
-                    "limit_cw": "U0:ID2:FC2:R0:DI0",
-                    "limit_ccw": "U0:ID2:FC2:R0:DI1"
+                    "limit_cw": "U0:ID16:FC2:R0:DI4",
+                    "limit_ccw": "!U0:ID16:FC2:R0:DI6"
                 }
             }
         ],
@@ -3157,10 +3157,10 @@ def __add_registers(args):
                     "close_on_shutdown": True,
                     "wait_on_shutdown": False,
                     "io_mode": 1, # 1: "single_out", 2: "dual_out"
-                    "output_cw": "U0:ID2:FC5:R0:RO7",
+                    "output_cw": "U0:ID16:FC5:R0:RO6",
                     "output_ccw": "off",
-                    "limit_cw": "U0:ID2:FC2:R0:DI2",
-                    "limit_ccw": "U0:ID2:FC2:R0:DI1"
+                    "limit_cw": "U0:ID16:FC2:R0:DI6",
+                    "limit_ccw": "!U0:ID16:FC2:R0:DI4"
                 }
             }
         ]
@@ -3199,8 +3199,8 @@ def __add_registers(args):
         {
             "uart": 1,
             "mb_id": 1,
-            "e_stop": "U0:ID2:FC5:R0:RO8",
-            "e_status": "U0:ID2:FC2:R0:DI0",
+            "e_stop": "U0:ID16:FC5:R0:RO7",
+            "e_status": "U0:ID16:FC2:R0:DI7",
             "stop_on_shutdown": True,
             "wait_on_shutdown": False
         }
