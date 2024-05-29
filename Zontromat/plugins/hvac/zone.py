dbcdf799f428d3e7b1e7631831bebbd4c4e7f8c4
Orlin Dimitrov <orlin369@gmail.com>
2024-05-29 11:34:27 +0300

    Add fixes

--- Zontromat/plugins/hvac/zone.py
+++ Zontromat/plugins/hvac/zone.py
@@ -1322,11 +1322,11 @@ class Zone(BasePlugin):
             print(f"Target: {self.__adjust_temp:2.1f}; Current: {self.__temp_proc.value:2.1f}")
 
             # Calculate the delta.
-            dt = self.__adjust_temp - self.__temp_proc.value
+            # dt = self.__adjust_temp - self.__temp_proc.value
 
             # Calculate the delta t and temperature deviation.
-            dt = min(self.__temp_proc.value-self.__adjust_temp+self.__temperature_deviation, 0) \
-                + max(self.__temp_proc.value-self.__adjust_temp-self.__temperature_deviation, 0)
+            dt = min(self.__adjust_temp - self.__temp_proc.value+self.__temperature_deviation, 0) \
+                + max(self.__adjust_temp - self.__temp_proc.value-self.__temperature_deviation, 0)
 
             # Round to have clear rounded value for state machine currency.
             dt = self.__round_to_nearest_half(dt)
